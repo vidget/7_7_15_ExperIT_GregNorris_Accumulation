59a4e2dabe99f9847d1e0cf66695078b185f5234
Vidget <vidget@yahoo.com>
2015-07-09 11:15:45 -0400

    Bank Account Accumulation

--- Program.cs
+++ Program.cs
@@ -10,19 +10,32 @@ namespace _7_7_15_ExperIT_GregNorris_Accumulation
     {
         static void Main(string[] args)
         {
-            double dailyDeposit = 35.00;
-            double[] days = { 365, 731, 1826,3653 };
-            double[] totals = { 0, 0,0, 0 };
 
+
+            //This program calculates the totals if a person deposits $35.00 per day of the course of 1, 2, 5, 10 years starting in Jan 2015
+
+            //dailyDeposit holds the $35.00 amount
+
+            decimal dailyDeposit = 35.00M;
+
+            //The array days contains the total number of days in each of the 4 different years 1st year = 365 2nd year = 731 the total for 2 years accounting for leapyear in 2016
+            //5 years total days = 1826 days , 10 years = 3653 acconting for 3 days for leapyears (2016,2020,2024)
+            decimal[] days = {365,731,1826,3653};
+            //an array created to hold the total values once calculated
+            decimal[] totals = {0,0,0,0};
+
+
+            //I used a for loop to cycle through the four different values held in the days and multiplied those times the dailyDepost($35.00) and put that value into the array called totals.
             for (int y = 0; y < 4; y++)
                 {
                     totals[y]= dailyDeposit * days[y];
                 }
                                   
+            //Prints out the values from the each total in the totals[] array
                      Console.WriteLine("The total deposits for 1 year "+ totals[0].ToString("C2"));
-                     Console.WriteLine("The total deposits for 2 year " + totals[1].ToString("C2"));
-                     Console.WriteLine("The total deposits for 5 year " + totals[2].ToString("C2"));
-                     Console.WriteLine("The total deposits for 10 year " + totals[3].ToString("C2"));
+                     Console.WriteLine("The total deposits for 2 years " + totals[1].ToString("C2"));
+                     Console.WriteLine("The total deposits for 5 years " + totals[2].ToString("C2"));
+                     Console.WriteLine("The total deposits for 10 years " + totals[3].ToString("C2"));
 
 
             Console.ReadLine();
