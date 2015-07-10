b252c43e390c78aa2d7ff0b1f30976fe05920a44
Vidget <vidget@yahoo.com>
2015-07-10 10:42:31 -0400

    Deposit 35

--- Program.cs
+++ Program.cs
@@ -18,11 +18,17 @@ namespace _7_7_15_ExperIT_GregNorris_Accumulation
 
             decimal dailyDeposit = 35.00M;
 
+            
+
             //The array days contains the total number of days in each of the 4 different years 1st year = 365 2nd year = 731 the total for 2 years accounting for leapyear in 2016
             //5 years total days = 1826 days , 10 years = 3653 acconting for 3 days for leapyears (2016,2020,2024)
             decimal[] days = {365,731,1826,3653};
             //an array created to hold the total values once calculated
             decimal[] totals = {0,0,0,0};
+
+           // String[] names = { "tom", "john" };
+
+           // int[] numbers = { 1, 2, 3 };
 
 
             //I used a for loop to cycle through the four different values held in the days and multiplied those times the dailyDepost($35.00) and put that value into the array called totals.
@@ -30,6 +36,13 @@ namespace _7_7_15_ExperIT_GregNorris_Accumulation
                 {
                     totals[y]= dailyDeposit * days[y];
                 }
+
+
+            //totals[0] = dailyDeposit * days[0];
+            //totals[1] = dailyDeposit * days[1];
+            //totals[2] = dailyDeposit * days[2];
+            //totals[3] = dailyDeposit * days[3];
+           
                                   
             //Prints out the values from the each total in the totals[] array
                      Console.WriteLine("The total deposits for 1 year "+ totals[0].ToString("C2"));
@@ -37,9 +50,10 @@ namespace _7_7_15_ExperIT_GregNorris_Accumulation
                      Console.WriteLine("The total deposits for 5 years " + totals[2].ToString("C2"));
                      Console.WriteLine("The total deposits for 10 years " + totals[3].ToString("C2"));
 
+                  
 
             Console.ReadLine();
 
-        }
+        } 
     }
 }
